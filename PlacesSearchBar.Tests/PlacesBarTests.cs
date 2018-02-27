2fde4c6e6572f59ee9841d44f416485183c1c047
Alex Smith <alexander.smith@just-eat.com>
2018-02-27 10:04:11 +0000

    Added unit test project

---
+++ PlacesSearchBar.Tests/PlacesBarTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+namespace PlacesSearchBar.Tests
+{
+    [TestFixture]
+    public class PlacesBarTests
+    {
+        [Test]
+        public void CreatePredictionsUriTest()
+        {
+        }
+    }
+}
